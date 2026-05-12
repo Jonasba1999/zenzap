4da35a925e44b4dfe6c327e39a39ed3bf76871ac
Jonas <jonasbal1999@gmail.com>
2026-05-12 15:41:28 +0300

    updated sticky cta with trigger

--- src/js/modules/stickyCta.ts
+++ src/js/modules/stickyCta.ts
@@ -2,15 +2,16 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export function stickyCta(): void {
-  const stickyCta = document.querySelector<HTMLElement>('[data-sticky-cta]');
+  const stickyCta = document.querySelector<HTMLElement>('[data-sticky-cta="cta"]');
+  const trigger = document.querySelector<HTMLElement>('[data-sticky-cta="trigger"]');
 
-  if (!stickyCta) return;
+  if (!stickyCta || !trigger) return;
 
   gsap.set(stickyCta, { yPercent: 100, autoAlpha: 0 });
 
   ScrollTrigger.create({
-    trigger: document.body,
-    start: '15% top',
+    trigger: trigger,
+    start: 'bottom top',
     onEnter: () => {
       gsap.timeline().set(stickyCta, { autoAlpha: 1 }).to(stickyCta, {
         yPercent: 0,
